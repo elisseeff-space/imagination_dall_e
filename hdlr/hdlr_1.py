5aed60cac075a15a049d04c7b26f9f878aa9b5a6
Pavel.Elisseeff <pavel.eliseev@gmail.com>
2024-01-28 17:45:08 +0300

    1

--- hdlr/hdlr_1.py
+++ hdlr/hdlr_1.py
@@ -157,12 +157,12 @@ async def message_with_photo(message: Message):
     user_first_name = message.from_user.first_name
     user_last_name = message.from_user.last_name
     user_name = message.from_user.username
-    if user_id is not None : user_name = str(user_id)
-    else : user_name = ''
+    if user_id is None : user_name_full = ''
+    else : user_name_full = str(user_id)
     #user_name = str(user_id)+'.'+user_username+'.'+user_first_name+'.'+user_last_name
-    if user_name is not None : user_name += user_name
-    if user_first_name is not None : user_name += user_first_name
-    if user_last_name is not None : user_name += user_last_name
+    if user_name is not None : user_name_full += user_name
+    if user_first_name is not None : user_name_full += user_first_name
+    if user_last_name is not None : user_name_full += user_last_name
     now = datetime.now()
     formatted_date = now.strftime("%Y-%m-%d %H:%M:%S")
 
